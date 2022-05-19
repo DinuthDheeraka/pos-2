6fe80436443909a6f42b7e4291bc07583cd2fda2
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-20 02:18:24 +0530

    Added SuperBO

--- POS/src/bo/BOFactory.java
+++ POS/src/bo/BOFactory.java
@@ -1,4 +1,18 @@
 package bo;
 
 public class BOFactory {
+
+    private static BOFactory boFactory;
+
+    private BOFactory(){}
+
+    public static BOFactory getBoFactory(){
+        return boFactory==null? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum getBO{
+        CUSTOMERBOIMPL
+    }
+
+
 }
