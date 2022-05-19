0263fc775089bb33bc8ee9aab0273a0b23ed042d
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-20 00:27:03 +0530

    Added Customer-Form Fieleds

--- POS/src/controller/CustomerFormController.java
+++ POS/src/controller/CustomerFormController.java
@@ -1,11 +1,35 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import util.NavigateUI;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CustomerFormController {
+public class CustomerFormController implements Initializable {
+    public TableView customerTbl;
+    public TableColumn colCustId;
+    public TableColumn colCustTitle;
+    public TableColumn colCustName;
+    public TableColumn colCustAddress;
+    public TableColumn colCustCity;
+    public TableColumn colCustProvince;
+    public TableColumn colCustPostalCode;
+    public TableColumn colCustJoinedDate;
+    public TextField txtCustSearchBar;
+    public Label lblCustCount;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
     public void addNewCustomerOnAction(ActionEvent actionEvent) {
         try {
             NavigateUI.getNavigateUI().setNewStage("Add-Customer-Form");
