36f95f59a198498811a7b1e1d17b1c10173ea300
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-20 01:40:55 +0530

    Added getAll method

--- POS/src/dao/custom/impl/CustomerDAOImpl.java
+++ POS/src/dao/custom/impl/CustomerDAOImpl.java
@@ -1,4 +1,28 @@
 package dao.custom.impl;
 
-public class CustomerDAOImpl {
+import dao.custom.CustomerDAO;
+import dto.CustomerDTO;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public ArrayList<CustomerDTO> getAll() throws SQLException, ClassNotFoundException {
+        ArrayList<CustomerDTO> customerDTOS = new ArrayList();
+        ResultSet resultSet = CrudUtil.execute("SELECT * FROM Customer;");
+        while (resultSet.next()){
+            customerDTOS.add(new CustomerDTO(
+                    resultSet.getString("CustID"),resultSet.getString("CusTitle"),
+                    resultSet.getString("CustName"),resultSet.getString("CustAddress"),
+                    resultSet.getString("City"),resultSet.getString("Province"),
+                    resultSet.getString("PostalCode"),
+                    LocalDate.parse(String.valueOf(resultSet.getDate("JoinedDate")))
+            ));
+        }
+        return customerDTOS;
+    }
 }
