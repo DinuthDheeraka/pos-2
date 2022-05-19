72ac9eaae4195502592911310406aad62a96300f
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-20 01:24:37 +0530

    Added CustomerDAOImpl

--- POS/src/dao/custom/CustomerDAO.java
+++ POS/src/dao/custom/CustomerDAO.java
@@ -1,6 +1,7 @@
 package dao.custom;
 
 import dao.CrudDAO;
+import dto.CustomerDTO;
 
-public interface CustomerDAO extends CrudDAO {
+public interface CustomerDAO extends CrudDAO<CustomerDTO,String> {
 }
