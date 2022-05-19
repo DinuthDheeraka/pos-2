7cd53de9a1f00b38ad99b4240e8458ac189c8203
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-20 01:48:46 +0530

    Added SuperDAO

--- POS/src/dao/CrudDAO.java
+++ POS/src/dao/CrudDAO.java
@@ -3,7 +3,7 @@ package dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface CrudDAO <DTO,ID>{
+public interface CrudDAO <DTO,ID> extends SuperDAO{
 
     public ArrayList<DTO> getAll() throws SQLException,ClassNotFoundException;
 }
--- POS/src/dao/DAOFactory.java
+++ POS/src/dao/DAOFactory.java
@@ -1,4 +1,14 @@
 package dao;
 
 public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getDaoFactory(){
+        return daoFactory==null? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+
 }
