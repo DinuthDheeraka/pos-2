42ae157936fd6a27ee5a45690555f3ef0210ea16
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-20 05:42:29 +0530

    Added generateId Method

--- POS/src/util/IdsGenerator.java
+++ POS/src/util/IdsGenerator.java
@@ -1,4 +1,13 @@
 package util;
 
 public class IdsGenerator {
+    public static String generateId(String prefix,String lastId){
+        if(lastId!=null){
+            int newCustomerId = Integer.parseInt(lastId.replace(prefix, "")) + 1;
+            return String.format(prefix+"%03d", newCustomerId);
+        }
+        else {
+            return prefix+000;
+        }
+    }
 }
