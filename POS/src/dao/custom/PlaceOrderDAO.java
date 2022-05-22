282dd15f0879327092a16746a1c4e6af1b10c8d7
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-22 18:08:32 +0530

    Added insertOrder(PlaceOrderDAOImpl)

--- POS/src/dao/custom/PlaceOrderDAO.java
+++
@@ -1,7 +0,0 @@
-package dao.custom;
-
-import dao.CrudDAO;
-import dto.OrdersDTO;
-
-public interface PlaceOrderDAO extends CrudDAO<OrdersDTO,String> {
-}
--- POS/src/dao/custom/impl/PlaceOrderDAOImpl.java
+++ POS/src/dao/custom/impl/PlaceOrderDAOImpl.java
@@ -1,4 +1,49 @@
 package dao.custom.impl;
 
-public class PlaceOrderDAOImpl {
+import dao.custom.OrderDAO;
+import dto.OrdersDTO;
+import entity.Orders;
+import util.CrudUtil;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PlaceOrderDAOImpl implements OrderDAO {
+
+    @Override
+    public ArrayList<Orders> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void insert(Orders orders) throws SQLException, ClassNotFoundException {
+        if(CrudUtil.execute("INSERT INTO Orders VALUES(?,?,?)",
+                orders.getOrderId(),orders.getDate(),orders.getCustID())){
+        }
+    }
+
+    @Override
+    public void delete(String s) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public void update(Orders orders) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public ArrayList<String> getAllIds() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Orders get(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public String getLastId() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
