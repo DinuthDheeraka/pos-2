e7c50e3c4457f5cccf188cb7edab8bc52bb4e696
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-22 18:37:30 +0530

    OrderDetailDAOImpl

---
+++ POS/src/dao/custom/OrderDetailDAO.java
@@ -0,0 +1,7 @@
+package dao.custom;
+
+import dao.CrudDAO;
+import entity.OrderDetail;
+
+public interface OrderDetailDAO extends CrudDAO<OrderDetail,String> {
+}
--- POS/src/dao/custom/impl/OrderDetailDAO.java
+++
@@ -1,4 +0,0 @@
-package dao.custom.impl;
-
-public interface OrderDetailDAO {
-}
