6c990dfd5ce1b0a79e88fff98adbaa78cba9f312
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-05-27 22:20:52 +0530

    DI(JoinQueryBOImpl)

--- POS/src/lk/ijse/pos/controller/IncomeReportsFormController.java
+++ POS/src/lk/ijse/pos/controller/IncomeReportsFormController.java
@@ -6,6 +6,38 @@
  */
 package lk.ijse.pos.controller;
 
-public class IncomeReportsFormController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import lk.ijse.pos.bo.custom.JoinQueryBO;
+import lk.ijse.pos.bo.custom.impl.JoinQueryBOImpl;
 
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class IncomeReportsFormController implements Initializable {
+
+    public TextField txtSearchBar;
+    public Label txtTotalIncome;
+    public Label txtTotalDiscount;
+    public Label txtMonth;
+    public Label txtMonthlyIncome;
+    public LineChart incomeChart;
+
+    private String year;
+
+    JoinQueryBO joinQueryBO = new JoinQueryBOImpl();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        year = String.valueOf(LocalDate.now().getYear());
+    }
+
+    public void txtSearchBarOnAction(ActionEvent actionEvent) {
+
+    }
 }
